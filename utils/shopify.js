4a7efc598a5b9f94a9575c162b56ea47bd69e0d9
Ali  Alizada <akbar.alizadeh5@gmail.com>
2021-01-22 15:14:11 -0800

    upload the website

--- utils/shopify.js
+++ utils/shopify.js
@@ -1,5 +1,5 @@
 import Client from "shopify-buy";
 export const client = Client.buildClient({
-  storefrontAccessToken: "f2e75d6cd4233c722df9e32d52b158d8",
-  domain: "ayakoalizada.myshopify.com",
+  storefrontAccessToken: "",
+  domain: "",
 });
